1f5b09ffbd1f3dce96709548f58d35f4ca93f285
Billy Yuan <billy.yuan1@gmail.com>
2024-03-06 16:53:49 +0900

    BY4 implement cache (#4)
    * BY4 install redis
    
    * BY4 Implement get in redis
    
    * BY4 add scaffiene cache
    
    * BY4 save to cache if pair is not in cache
    
    * BY4 save to cache in a single transaction
    
    * BY4 add RatesCache to constructor
    
    * BY6 make scaffiene private

--- forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
+++ forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
@@ -5,5 +5,5 @@ import interpreters._
 import forex.thirdPartyApi.oneFrameApiClient
 
 object Interpreters {
-  def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F](oneFrameApiClient)
+  def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F](oneFrameApiClient, RatesCache.ratesCache)
 }
