575ed8aa95ef01d3a15246f26ec8c715660d772d
Billy Yuan <billy.yuan1@gmail.com>
2024-03-07 08:06:10 +0900

    BY3 WIP add api response if query param not found

--- forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -8,18 +8,27 @@ import forex.programs.rates.{ Protocol => RatesProgramProtocol }
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
+import cats.data.Validated.Invalid
+import cats.data.Validated.Valid
 
 class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
-  import Converters._, QueryParams._, Protocol._
+  import Converters._, QueryParams._
 
   private[http] val prefixPath = "/rates"
 
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
-    case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
-      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
-        Ok(rate.asGetApiResponse)
+    case GET -> Root :? FromQueryParam(maybeFrom) +& ToQueryParam(maybeTo) =>
+      maybeFrom match {
+        case Invalid(_) => BadRequest("TODO: Invalid from JSON")
+        case Valid(from) => 
+          maybeTo match {
+            case Invalid(_) => BadRequest("TODO: Invalid to JSON")
+            case Valid(to) =>
+              rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate => Ok(rate.asGetApiResponse)}
+          }
       }
+
   }
 
   val routes: HttpRoutes[F] = Router(
